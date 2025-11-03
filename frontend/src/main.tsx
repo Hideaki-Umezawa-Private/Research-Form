6d49592aa551272817329fc26d167709d9383dce
Hideaki-Umezawa-Private <hukuzawayukitiiiii@gamil.com>
2025-11-04 00:19:49 +0900

    wip:loginform作成途中

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {App} from './App.tsx';
 import './index.css';
+// import {App} from '@/App.tsx';
+import {LoginPage} from '@/page/LoginPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/*<App />*/}
+      <LoginPage/>
   </StrictMode>,
 )
