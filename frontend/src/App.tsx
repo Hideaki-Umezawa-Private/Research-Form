9d732379d1286e72b825c55d6f0152dcd22d8bce
Hideaki-Umezawa-Private <hukuzawayukitiiiii@gamil.com>
2025-11-03 23:23:55 +0900

    setup:tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import {useAsync} from 'react-use';
-import axios from 'axios';
-import {useState} from 'react';
+import {useAsync} from 'react-use'
+import axios from 'axios'
+import {useState} from 'react'
 
 export const App = () => {
     const [getHelloWorld, setGetHelloWorld] = useState<string>('');
@@ -11,6 +11,8 @@ export const App = () => {
 
     return (
         <>
+            <h1>Tailwind Ready!</h1>
+            <h1 className="text-3xl font-bold text-red-500"> Tailwind Ready! </h1>
             <div>{getHelloWorld}</div>
         </>
     )
